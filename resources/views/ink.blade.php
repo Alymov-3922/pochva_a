623331f426127750edce8e965c0309e450d33e7f
Alymov-3922 <alymovadilet1996@gmail.com>
2023-01-11 11:49:32 +0600

    'commit'

---
+++ resources/views/ink.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <ink-component></ink-component>
+@endsection
+@push('page-scripts')
+    <script>
+
+    </script>
+@endpush
---
+++ storage/framework/views/a4a9c0ddae16ea50ab3a764a1cfc02426fed97e8.php
@@ -0,0 +1,10 @@
+<?php $__env->startSection('content'); ?>
+    <ink-component></ink-component>
+<?php $__env->stopSection(); ?>
+<?php $__env->startPush('page-scripts'); ?>
+    <script>
+
+    </script>
+<?php $__env->stopPush(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\OSPanel\domains\ga\resources\views/ink.blade.php ENDPATH**/ ?>
